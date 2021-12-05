d1d7ef0f8b2d1f16d06abf791e978b0df2619c98
Guruprasada Shridhar Hegde <85961223+GuruprasadaShridharHegde@users.noreply.github.com>
2021-12-05 09:22:48 +0530

    Password validity Checker

---
+++ Task Solution 7/Password Validity Program.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec  5 09:11:59 2021
+
+@author: Guruprasada Shridhar Hegde
+"""
+import re
+p= input("Input your password")
+x = True
+while x:  
+    if (len(p)<6 or len(p)>16):
+        break
+    elif not re.search("[a-z]",p):
+        break
+    elif not re.search("[0-9]",p):
+        break
+    elif not re.search("[A-Z]",p):
+        break
+    elif not re.search("[#$%&'()*+,-./:;<=>?@[\]^_'{|}~]",p):
+        break
+    elif re.search("\s",p):
+        break
+    else:
+        print("Valid Password")
+        x=False
+        break
+
+if x:
+    print("Not a Valid Password")
+
+
